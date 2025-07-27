f7360a8c7f315d3bad6a7d401d97718dfa25139b
Enver <enver.erfanovich@gmail.com>
2025-07-27 16:08:41 +0300

    Added projects data

---
+++ data/projects.ts
@@ -0,0 +1,30 @@
+export const projects = [
+  {
+    id: 1,
+    title: "Макет сайта по подбору недвижимости",
+    subtitle: "Web Design",
+    color: "bg-purple-500",
+    image: "/img/translate.jpg",
+  },
+  {
+    id: 2,
+    title: "Макет сайта строительной компании",
+    subtitle: "Web Design",
+    color: "bg-green-500",
+    image: "/img/build.jpg",
+  },
+  {
+    id: 3,
+    title: "Senta",
+    subtitle: "Mobile App & Branding",
+    color: "bg-blue-500",
+    image: "/img/translate.jpg",
+  },
+  {
+    id: 4,
+    title: "Crint",
+    subtitle: "Mobile App",
+    color: "bg-orange-500",
+    image: "/img/translate.jpg",
+  },
+];
